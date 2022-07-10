67e6ae13ed09714bc9148b555e91d9250fc28ed2
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-07-10 12:59:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## MipmaplevelandLanguageFix
+
+Makes changing Mipmaplevels and Language faster/instant, by instead of doing a full reload, only reloading the respective part of the game.
+
+Improved Mipmaplevel reloading is not compatible with the sodium menu.
---
+++ src/main/java/com/kingcontaria/mipmaplevelandlanguagefix/mixin/VideoOptionsScreenMixin.java
@@ -0,0 +1,21 @@
+package com.kingcontaria.mipmaplevelandlanguagefix.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(VideoOptionsScreen.class)
+
+public class VideoOptionsScreenMixin {
+
+    @Redirect(method = "removed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;reloadResourcesConcurrently()Ljava/util/concurrent/CompletableFuture;"))
+    private CompletableFuture<Void> reloadMipMapLevels(MinecraftClient client) {
+        ((BakedModelManagerAccessor)client.getBakedModelManager()).callApply(((BakedModelManagerAccessor)client.getBakedModelManager()).callPrepare(client.getResourceManager(), client.getProfiler()), client.getResourceManager(), client.getProfiler());
+        return null;
+    }
+
+}
